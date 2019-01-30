e4cf149cf91ca2b81ff678ae463633e3fa350cc3
Masee Hussain <hussainmasee@gmail.com>
2019-01-30 10:03:11 +0000

    created and added content in my Stuff.js

---
+++ src/components/Stuff.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Stuff extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Stuff</h2>
+        <ol>
+          <li>dkjsljfljsdl</li>
+          <li>sfsdklfjksj</li>
+          <li>wpuwiou</li>
+          <li>mxcbmxcbm</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Stuff;
