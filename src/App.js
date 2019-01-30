95f943eeacc1c5d603425c74293fde0ff1a2110b
Masee Hussain <hussainmasee@gmail.com>
2019-01-30 10:32:10 +0000

    URL defined by the to prop acts as an identifier to load right content

--- src/App.js
+++ src/App.js
@@ -13,16 +13,20 @@ class App extends React.Component {
           <h1>Simple SPA</h1>
           <ul className="header">
             <li>
-              <NavLink href="/">Home</NavLink>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li>
-              <NavLink href="/stuff">Stuff</NavLink>
+              <NavLink to="/stuff">Stuff</NavLink>
             </li>
             <li>
-              <NavLink href="/contact">Contact</NavLink>
+              <NavLink to="/contact">Contact</NavLink>
             </li>
           </ul>
-          <div className="content" />
+          <div className="content">
+            <Route path="/" component={Home} />
+            <Route path="/stuff" component={Stuff} />
+            <Route path="/contact" component={Contant} />
+          </div>
         </div>
       </HashRouter>
     );
