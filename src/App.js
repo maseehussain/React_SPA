69503bec0222b85d2d14a250fe09974377131fdf
Masee Hussain <hussainmasee@gmail.com>
2019-01-30 09:38:40 +0000

    displayed content

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li>Home</li>
+          <li>Stuff</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
