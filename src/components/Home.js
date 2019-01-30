01121a9db0e916cc71b3e666d928a62899ac089a
Masee Hussain <hussainmasee@gmail.com>
2019-01-30 09:50:55 +0000

    rendered content on Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Hey!!!</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+          obcaecati dolor repudiandae at quae? Quae incidunt cum dolor tempora
+          sint, facere dignissimos nam aliquid reiciendis esse consequuntur,
+          adipisci officiis dolorem?
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
