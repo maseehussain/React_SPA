43807de9bd6b9952cd95821338923998587c3ba9
Masee Hussain <hussainmasee@gmail.com>
2019-01-30 10:16:21 +0000

    created contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Got questions?</h2>
+        <p>
+          <a mailto="masee@masee.com">Masee</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
